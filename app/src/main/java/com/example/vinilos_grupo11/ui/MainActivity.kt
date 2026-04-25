f96da0f13475e58f0eedf74733efd7cac0dcfe88
MarcelaHernandezL <mv.hernandezl1@uniandes.edu.co>
2026-04-25 16:53:18 -0500

    Cambios en la interfaz de Albums para adaptarla al prototipo

--- app/src/main/java/com/example/vinilos_grupo11/ui/MainActivity.kt
+++ app/src/main/java/com/example/vinilos_grupo11/ui/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.vinilos_grupo11.ui
 
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
@@ -13,12 +14,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
 
+        // Configurar Bottom Navigation
         val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav)
         bottomNav.setupWithNavController(navController)
+
+        // Configurar Botón de Retroceso de la Top Bar
+        val btnBack = findViewById<ImageButton>(R.id.btn_back)
+        btnBack.setOnClickListener {
+            // Si no hay más fragmentos en el stack, cerramos la actividad para volver al Home
+            if (!navController.navigateUp()) {
+                finish()
+            }
+        }
     }
 }
